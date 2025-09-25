c3b73ec9009b164c36f1ecddb04050ca29b5e026
Giordano Borsato <borsatogiordano@gmail.com>
2025-09-25 15:08:09 -0300

    feat: update package.json with Vitest configuration and add new test scripts
    refactor: update import paths to use alias for cleaner code
    
    chore: add Vitest configuration files for unit and e2e testing
    
    fix: adjust TypeScript configuration for ES2023 and add path mapping

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    exclude: ["**/*.spec.ts", "node_modules", "dist"],
+    root: './',
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+})
